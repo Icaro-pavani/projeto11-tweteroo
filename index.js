de6af8f2c6d2ea4ca078fb2f33cedd9d4f330a16
Icaro-pavani <icaro.pavani79@gmail.com>
2022-04-23 13:34:32 -0300

    project finished

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ function validURL(str) {
 }
 
 const users = [];
-const tweets = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub",
-  },
-];
+const tweets = [];
 
 const app = express();
 
@@ -29,11 +22,21 @@ app.use(express.json());
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   if (username.length !== 0 && avatar.length !== 0) {
-    if (validURL(avatar)) {
-      users.push(req.body);
-      res.send("OK");
+    const indexUser = users.findIndex((user) => user.username === username);
+    if (indexUser === -1) {
+      if (validURL(avatar)) {
+        users.push(req.body);
+        res.send("OK");
+      } else {
+        res.status(400).send("Adicione um link válido para a imagem!");
+      }
     } else {
-      res.status(400).send("Adicione um link válido para a imagem!");
+      if (validURL(avatar)) {
+        users[indexUser].avatar = avatar;
+        res.send("OK");
+      } else {
+        res.status(400).send("Adicione um link válido para a imagem!");
+      }
     }
   } else {
     res.status(400).send("Todos os campos são obrigatórios!");
@@ -43,18 +46,15 @@ app.post("/sign-up", (req, res) => {
 app.get("/tweets", (req, res) => {
   const { page } = req.query;
   if (Number(page) > 0) {
-    if (tweets.length < 11) {
-      const tweetsToSend = [...tweets];
-      res.send(tweetsToSend.reverse());
+    let tweetsToSend = [];
+    if (Number(page) === 1) {
+      tweetsToSend = tweets.slice(-page * 10, tweets.length);
     } else {
-      let tweetsToSend = [];
-      if (Number(page) === 1) {
-        tweetsToSend = tweets.slice(-page * 10, tweets.length);
-      } else {
+      if (tweets.length > (page - 1) * 10) {
         tweetsToSend = tweets.slice(-page * 10, -(page - 1) * 10);
       }
-      res.send(tweetsToSend.reverse());
     }
+    res.send(tweetsToSend.reverse());
   } else {
     res.status(400).send("Informe uma página válida!");
   }
